8a59d3efd9178e8c0bc45200e5606738e634adf1
Juan José Peinado Rufián <juanjotrick@gmail.com>
2018-03-15 19:38:56 +0100

    Add files via upload

---
+++ LimpiarWin10.ps1
@@ -0,0 +1,25 @@
+
+# Alarms & Clocks: 
+get-appxpackage Microsoft.WindowsAlarms | remove-appxpackage
+# Calculator: 
+get-appxpackage Microsoft.WindowsCalculator | remove-appxpackage
+# Camera: 
+get-appxpackage Microsoft.WindowsCamera | remove-appxpackage
+# Groove Music: 
+get-appxpackage Microsoft.ZuneMusic | remove-appxpackage
+# Mail & Calendar: 
+get-appxpackage icrosoft.windowscommunicationsapps | remove-appxpackage
+# Maps: 
+get-appxpackage Microsoft.WindowsMaps | remove-appxpackage
+# Movies & Tv: 
+get-appxpackage Microsoft.ZuneVideo | remove-appxpackage
+# OneNote: 
+get-appxpackage Microsoft.Office.OneNote | remove-appxpackage
+# People: 
+get-appxpackage Microsoft.People | remove-appxpackage
+# Photos: 
+get-appxpackage Microsoft.Windows.Photos | remove-appxpackage
+# Voice Recorder: 
+get-appxpackage Microsoft.WindowsSoundRecorder | remove-appxpackage
+# Xbox: 
+get-appxpackage Microsoft.XboxApp | remove-appxpackage
